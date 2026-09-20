3b6647afc4cffd199b740cd924c6b4fa66ef5205
Luthfia Agustina <2410010202luthfia@gmail.com>
2026-09-20 20:10:28 +0700

    latihan mandiri membuat kartu mahasiswa

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/halo/java/KartuMahasiswa.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01.halo.java;
+
+/**
+ * Program pertama PBO 2 untuk memastikan JSK, NetBeans, dan Maven siap digunakan
+ * @author ACER
+ */
+public class KartuMahasiswa {
+    
+    public static void main (String[] srgs) {
+        String nama = "Siti Rahmah";
+        String NPM = "2410010123";
+        String Prodi = "Teknik Informatika";
+        String Semester = "5";
+        String Alasan = "Ingin membuat aplikasi kasir";
+        
+        System.out.println("=========================");
+        System.out.println("KARTU MAHASISWA PBO 2");
+        System.out.println("=========================");
+        System.out.println("Nama        : " + ("Luthfia Agustina"));
+        System.out.println("NPM         : " + ("2410010202"));
+        System.out.println("Prodi       : " + ("Teknik Informatika"));
+        System.out.println("Semester    : " + ("5"));
+        System.out.println("Alasan      : " + ("Ingin lebih memahami pemrograman, terutama dalam membuat aplikasi"));
+        System.out.println("==========================");
+    }
+}
